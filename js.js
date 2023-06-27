499db1e934a5150a099e99a6d14c128be52cbf11
Jeniffer Ponce <Jenifeerponce2000.17@gmail.com>
2023-06-27 11:09:37 -0500

    Tiempo1 inicio de programa

---
+++ js.js
@@ -0,0 +1,10 @@
+function dibujarYCalcularDistancia() {
+
+var x1 = parseInt(document.getElementById("x1").value);
+var y1 = parseInt(document.getElementById("y1").value);
+var x2 = parseInt(document.getElementById("x2").value);
+var y2 = parseInt(document.getElementById("y2").value);
+
+r=x1+x2+y1+y2
+document.write("La suma es: "+r);
+}
